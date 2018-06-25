3144e254ad584f6e65ba33384fd35d985037e9c5
oyar <hamuyrodrigo@gmail.com>
2018-06-25 21:00:47 +0100

    Indent: RotateComponent.cs

--- Assets/script/components/RotateComponent.cs
+++ Assets/script/components/RotateComponent.cs
@@ -1,13 +1,13 @@
-using UnityEngine.Events;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class RotateComponent : MonoBehaviour {
 
 	RotateHandleComponent handle;
 
-	public UnityEvent onRotationDone = new UnityEvent();
-	public UnityEvent onRotationStart = new UnityEvent();
-	public UnityEvent onCanRotateChange = new UnityEvent();
+	public UnityEvent onRotationDone = new UnityEvent ();
+	public UnityEvent onRotationStart = new UnityEvent ();
+	public UnityEvent onCanRotateChange = new UnityEvent ();
 
 	Vector3 startDir;
 
@@ -15,51 +15,51 @@ public class RotateComponent : MonoBehaviour {
 
 	public bool canRotate = true;
 
-	void Start(){
+	void Start () {
 
 		// Set points as rotatable
-		var containerComponents = GetComponentsInChildren<PointsContainerComponent>();
-		foreach( var containerComponent in containerComponents ){
+		var containerComponents = GetComponentsInChildren<PointsContainerComponent> ();
+		foreach (var containerComponent in containerComponents) {
 			var container = containerComponent.pathContainer;
 
-			setPointsAsRotatable(container);
+			setPointsAsRotatable (container);
 
-			container.onGeneratePathPointsDone.AddListener( () => {
-				setPointsAsRotatable(container);
+			container.onGeneratePathPointsDone.AddListener (() => {
+				setPointsAsRotatable (container);
 			});
 
 			// Update points before/after rotation
-			onRotationDone.AddListener(container.ResetPoints);
-			onRotationStart.AddListener(container.onRotationStart);
+			onRotationDone.AddListener (container.ResetPoints);
+			onRotationStart.AddListener (container.onRotationStart);
 		}
 
-		handle = GetComponentsInChildren<RotateHandleComponent>()[0];
-		handle.onMouseDown.AddListener(OnHandleMouseDown);
+		handle = GetComponentsInChildren<RotateHandleComponent> () [0];
+		handle.onMouseDown.AddListener (OnHandleMouseDown);
 
-		var player = Object.FindObjectOfType< PlayerComponent >();
+		var player = Object.FindObjectOfType<PlayerComponent> ();
 
-        if (player == null) return;
+		if (player == null) return;
 
 		// Enable/disable rotation during player movement.
-		player.onTargetReached.AddListener( () => {
+		player.onTargetReached.AddListener (() => {
 			canRotate = true;
-			onCanRotateChange.Invoke();
+			onCanRotateChange.Invoke ();
 		});
-		player.onStartMoving.AddListener( () => {
+		player.onStartMoving.AddListener (() => {
 			canRotate = false;
-			onCanRotateChange.Invoke();
+			onCanRotateChange.Invoke ();
 		});
 	}
 
-	void setPointsAsRotatable( PathContainer container ){
-		foreach( var point in container.points ) {
+	void setPointsAsRotatable (PathContainer container) {
+		foreach (var point in container.points) {
 			point.canMove = true;
 		}
 	}
 
-	void Update(){
+	void Update () {
 
-		if ( canRotate ) CheckInput();
+		if (canRotate) CheckInput ();
 	}
 
 	// public void Rotate(){
@@ -74,34 +74,32 @@ public class RotateComponent : MonoBehaviour {
 
 	bool isTouching = false;
 
-	void OnHandleMouseDown(){
+	void OnHandleMouseDown () {
 		isTouching = true;
 	}
 
+	void CheckInput () {
 
-	void CheckInput(){
+		var touchPhase = Utility.getTouchPhase ();
 
-		var touchPhase = Utility.getTouchPhase();
-
-		if( 
-			isTouching && 
+		if (
+			isTouching &&
 			touchPhase != TouchPhase.Canceled &&
 			(
 				isRotating != (touchPhase == TouchPhase.Began)
 			)
 		) {
 
-			var touchPos = Utility.getTouch();
+			var touchPos = Utility.getTouch ();
 
-			OnTouch( touchPos, touchPhase == TouchPhase.Began );
+			OnTouch (touchPos, touchPhase == TouchPhase.Began);
 
-
-			if ( touchPhase == TouchPhase.Ended ) isTouching = false;
+			if (touchPhase == TouchPhase.Ended) isTouching = false;
 
 		} else {
 
-			if( isRotating ) {
-				SnapRotation();
+			if (isRotating) {
+				SnapRotation ();
 			}
 
 		}
@@ -112,13 +110,13 @@ public class RotateComponent : MonoBehaviour {
 	Vector3 targetForward;
 	bool isRotationTargetSet = false;
 	float rotationSpeed = 12;
-	void SnapRotation(){
+	void SnapRotation () {
 
-		if( ! isRotationTargetSet ) {
+		if (!isRotationTargetSet) {
 			var euler = transform.eulerAngles;
 
-			for (var i = 0; i < 3; i++ ) {
-				euler[i] = Mathf.Round(euler[i] / 90.0f ) * 90.0f;
+			for (var i = 0; i < 3; i++) {
+				euler[i] = Mathf.Round (euler[i] / 90.0f) * 90.0f;
 			}
 			snapRotation = Quaternion.identity;
 			snapRotation.eulerAngles = euler;
@@ -126,64 +124,63 @@ public class RotateComponent : MonoBehaviour {
 			isRotationTargetSet = true;
 		}
 
-		transform.rotation = Quaternion.Slerp(transform.rotation, snapRotation, 0.5f*rotationSpeed*Time.deltaTime);
+		transform.rotation = Quaternion.Slerp (transform.rotation, snapRotation, 0.5f * rotationSpeed * Time.deltaTime);
 
-		if( Vector3.Dot(transform.forward, targetForward) > 0.999999f ) {
+		if (Vector3.Dot (transform.forward, targetForward) > 0.999999f) {
 			transform.rotation = snapRotation;
 			isRotating = false;
 			isRotationTargetSet = false;
 
-			if( ! Utility.canPlayerMove ) {
+			if (!Utility.canPlayerMove) {
 				Utility.canPlayerMove = true;
-				onRotationDone.Invoke();
-				print("onRotationDone");
+				onRotationDone.Invoke ();
+				print ("onRotationDone");
 			}
 		}
 
 	}
 
-    float currAngle = 0;
+	float currAngle = 0;
 
-    void OnTouch(Vector3 screenTouchPosition, bool startPhase = false){
+	void OnTouch (Vector3 screenTouchPosition, bool startPhase = false) {
 
 		var camera = Camera.main;
 
-		var handleScreenPos = camera.WorldToScreenPoint( handle.transform.position );
+		var handleScreenPos = camera.WorldToScreenPoint (handle.transform.position);
 		handleScreenPos.z = 0;
 
 		var endDir = screenTouchPosition - handleScreenPos;
 		endDir.z = 0;
 
-		if ( startPhase ) {
-            currAngle = 0;
-            startDir = endDir;
-			isRotating =  true;
+		if (startPhase) {
+			currAngle = 0;
+			startDir = endDir;
+			isRotating = true;
 			isRotationTargetSet = false;
 			return;
 		}
 
+		var angle = Vector3.SignedAngle (startDir, endDir, Vector3.forward);
 
-        var angle = Vector3.SignedAngle(startDir, endDir, Vector3.forward);
-
-		if( Utility.canPlayerMove && Mathf.Abs(angle) > 0.1f ) {
-			onRotationStart.Invoke();
+		if (Utility.canPlayerMove && Mathf.Abs (angle) > 0.1f) {
+			onRotationStart.Invoke ();
 			Utility.canPlayerMove = false;
-			print("onRotationStart");
+			print ("onRotationStart");
 		}
 
-        var newAngle = Mathf.LerpAngle(currAngle, -angle, rotationSpeed * Time.deltaTime);
+		var newAngle = Mathf.LerpAngle (currAngle, -angle, rotationSpeed * Time.deltaTime);
 
-        transform.Rotate(handle.transform.up, newAngle - currAngle, Space.World);
-        currAngle = newAngle;
-    }
+		transform.Rotate (handle.transform.up, newAngle - currAngle, Space.World);
+		currAngle = newAngle;
+	}
 
-		Vector3 GetTouchPosition(){
-			if (Input.touchCount == 1) {
-				var touch = Input.GetTouch (0);
-				return touch.position;
-			} else if (Input.GetMouseButton (0)) {
-				return Input.mousePosition;
-			}
-			return Vector3.zero;
+	Vector3 GetTouchPosition () {
+		if (Input.touchCount == 1) {
+			var touch = Input.GetTouch (0);
+			return touch.position;
+		} else if (Input.GetMouseButton (0)) {
+			return Input.mousePosition;
 		}
+		return Vector3.zero;
+	}
 }
