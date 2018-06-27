b5171fac0e1652534e59679a100236e7e059f149
oyar <hamuyrodrigo@gmail.com>
2018-06-27 17:59:20 +0100

    Indent

--- Assets/Tests.cs
+++ Assets/Tests.cs
@@ -1,242 +1,227 @@
-using UnityEngine.SceneManagement;
-using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 // using UnityEditor;
-using UnityEngine.TestTools;
-using NUnit.Framework;
 using System.Collections;
 using System.Linq;
+using NUnit.Framework;
+using UnityEngine.TestTools;
 
 public class Tests {
 
-	[UnityTest]
-	public IEnumerator Issue002_000_ShouldNotFind() {
-		yield return LoadScene("issue-002.000");
-		Assert.IsFalse( HasPath(), "Should not find a path." );
-	}
+  [UnityTest]
+  public IEnumerator Issue002_000_ShouldNotFind () {
+    yield return LoadScene ("issue-002.000");
+    Assert.IsFalse (HasPath (), "Should not find a path.");
+  }
 
-	[UnityTest]
-	public IEnumerator Issue002_001_ShouldNotFind() {
-		yield return LoadScene("issue-002.001");
-        var path = GetPath();
-        Assert.AreEqual(new Vector3(-4.0f, 1.5f, -4.0f), path.Last().position, "Should be the right target.");
-	}
+  [UnityTest]
+  public IEnumerator Issue002_001_ShouldNotFind () {
+    yield return LoadScene ("issue-002.001");
+    var path = GetPath ();
+    Assert.AreEqual (new Vector3 (-4.0f, 1.5f, -4.0f), path.Last ().position, "Should be the right target.");
+  }
 
+  [UnityTest]
+  public IEnumerator Issue002_002_ShouldNotFind () {
+    yield return LoadScene ("issue-002.002");
+    Assert.IsFalse (HasPath (), "Should not find a path.");
+  }
 
-	[UnityTest]
-	public IEnumerator Issue002_002_ShouldNotFind() {
-		yield return LoadScene("issue-002.002");
-		Assert.IsFalse( HasPath(), "Should not find a path." );
-	}
+  [UnityTest]
+  public IEnumerator Issue002_003_ShouldFind () {
+    yield return LoadScene ("issue-002.003");
+    Assert.IsTrue (HasPath (), "Should find a path.");
+  }
 
-	[UnityTest]
-	public IEnumerator Issue002_003_ShouldFind() {
-		yield return LoadScene("issue-002.003");
-		Assert.IsTrue( HasPath(), "Should find a path." );
-	}
+  [UnityTest]
+  public IEnumerator Issue002_004Passes_ShoulNeed5Steps () {
+    yield return LoadScene ("issue-002.004");
+    Assert.AreEqual (5, GetPath ().Count, "Should need 5 steps.");
+  }
 
-	[UnityTest]
-	public IEnumerator Issue002_004Passes_ShoulNeed5Steps() {
-		yield return LoadScene("issue-002.004");
-		Assert.AreEqual( 5, GetPath().Count, "Should need 5 steps." );
-	}
+  [UnityTest]
+  public IEnumerator Issue002_005_ShouldNotFind () {
+    yield return LoadScene ("issue-002.005");
+    Assert.IsFalse (HasPath (), "Should not find a path.");
+  }
 
-	[UnityTest]
-	public IEnumerator Issue002_005_ShouldNotFind() {
-		yield return LoadScene("issue-002.005");
-		Assert.IsFalse( HasPath(), "Should not find a path." );
-	}
+  [UnityTest]
+  public IEnumerator Issue002_006_ShouldNotFind () {
+    yield return LoadScene ("issue-002.006");
+    Assert.IsFalse (HasPath (), "Should not find a path.");
+  }
 
-	[UnityTest]
-	public IEnumerator Issue002_006_ShouldNotFind() {
-		yield return LoadScene("issue-002.006");
-		Assert.IsFalse( HasPath(), "Should not find a path." );
-	}
+  [UnityTest]
+  public IEnumerator Issue005_000_ShouldNotFind () {
+    yield return LoadScene ("issue-005.000");
+    Assert.IsFalse (HasPath (), "Should not find a path.");
+  }
 
-	[UnityTest]
-	public IEnumerator Issue005_000_ShouldNotFind() {
-		yield return LoadScene("issue-005.000");
-		Assert.IsFalse( HasPath(), "Should not find a path." );
-	}
+  [UnityTest]
+  public IEnumerator Issue005_001_ShouldFind () {
+    yield return LoadScene ("issue-005.001");
+    Assert.IsTrue (HasPath (), "Should find a path.");
+  }
 
-	[UnityTest]
-	public IEnumerator Issue005_001_ShouldFind() {
-		yield return LoadScene("issue-005.001");
-		Assert.IsTrue( HasPath(), "Should find a path." );
-	}
+  [UnityTest]
+  public IEnumerator Issue009_000_ShouldFind () {
+    yield return LoadScene ("issue-009.000");
+    Assert.IsTrue (HasPath (), "Should find a path.");
+  }
 
-	[UnityTest]
-	public IEnumerator Issue009_000_ShouldFind() {
-		yield return LoadScene("issue-009.000");
-		Assert.IsTrue( HasPath(), "Should find a path." );
-	}
+  [UnityTest]
+  public IEnumerator Issue009_001_ShouldFind () {
+    yield return LoadScene ("issue-009.001");
+    Assert.IsTrue (HasPath (), "Should find a path.");
+  }
 
-	[UnityTest]
-	public IEnumerator Issue009_001_ShouldFind() {
-		yield return LoadScene("issue-009.001");
-		Assert.IsTrue( HasPath(), "Should find a path." );
-	}
+  [UnityTest]
+  public IEnumerator Issueprism_000_ShouldFind () {
+    yield return LoadScene ("issue-prism.000");
+    Assert.IsTrue (HasPath (), "Should find a path.");
+  }
 
-	[UnityTest]
-	public IEnumerator Issueprism_000_ShouldFind() {
-		yield return LoadScene("issue-prism.000");
-		Assert.IsTrue( HasPath(), "Should find a path." );
-	}
+  [UnityTest]
+  public IEnumerator Issueprism_001_ShouldFind () {
+    yield return LoadScene ("issue-prism.001");
+    Assert.IsTrue (HasPath (), "Should find a path.");
+  }
 
-	[UnityTest]
-	public IEnumerator Issueprism_001_ShouldFind() {
-		yield return LoadScene("issue-prism.001");
-		Assert.IsTrue( HasPath(), "Should find a path." );
-	}
+  [UnityTest]
+  public IEnumerator Issue010_000_ShouldFind () {
+    yield return LoadScene ("issue-010.000");
+    Assert.IsTrue (HasPath (), "Should find a path.");
+  }
 
-    [UnityTest]
-    public IEnumerator Issue010_000_ShouldFind()
-    {
-        yield return LoadScene("issue-010.000");
-        Assert.IsTrue(HasPath(), "Should find a path.");
-    }
+  [UnityTest]
+  public IEnumerator Issue010_001_ShouldFind () {
+    yield return LoadScene ("issue-010.001");
+    Assert.IsTrue (HasPath (), "Should find a path.");
+  }
 
-    [UnityTest]
-    public IEnumerator Issue010_001_ShouldFind()
-    {
-        yield return LoadScene("issue-010.001");
-        Assert.IsTrue(HasPath(), "Should find a path.");
-    }
+  [UnityTest]
+  public IEnumerator Issue010_002_ShouldFind () {
+    yield return LoadScene ("issue-010.002");
+    Assert.IsTrue (HasPath (), "Should find a path.");
+  }
 
-    [UnityTest]
-    public IEnumerator Issue010_002_ShouldFind()
-    {
-        yield return LoadScene("issue-010.002");
-        Assert.IsTrue(HasPath(), "Should find a path.");
-    }
+  [UnityTest]
+  public IEnumerator Issue010_003_ShouldFind () {
+    yield return LoadScene ("issue-010.003");
+    Assert.IsFalse (HasPath (), "Should not find a path.");
+  }
 
-    [UnityTest]
-    public IEnumerator Issue010_003_ShouldFind()
-    {
-        yield return LoadScene("issue-010.003");
-        Assert.IsFalse(HasPath(), "Should not find a path.");
-    }
+  [UnityTest]
+  public IEnumerator Issue010_004_ShouldFind () {
+    yield return LoadScene ("issue-010.004");
+    Assert.IsFalse (HasPath (), "Should not find a path.");
+  }
 
-    [UnityTest]
-    public IEnumerator Issue010_004_ShouldFind()
-    {
-        yield return LoadScene("issue-010.004");
-        Assert.IsFalse(HasPath(), "Should not find a path.");
-    }
+  [UnityTest]
+  public IEnumerator Issue010_005_ShouldFind () {
+    yield return LoadScene ("issue-010.005");
+    Assert.IsFalse (HasPath (), "Should not find a path.");
+  }
 
-    [UnityTest]
-    public IEnumerator Issue010_005_ShouldFind()
-    {
-        yield return LoadScene("issue-010.005");
-        Assert.IsFalse(HasPath(), "Should not find a path.");
-    }
+  [UnityTest]
+  public IEnumerator Issue010_006_ShouldFind () {
+    yield return LoadScene ("issue-010.006");
+    var path = GetPath ();
+    Assert.AreEqual (new Vector3 (6.0f, -2.5f, 10.0f), path.Last ().position, "Should be the right target.");
+  }
 
-    [UnityTest]
-    public IEnumerator Issue010_006_ShouldFind()
-    {
-        yield return LoadScene("issue-010.006");
-		var path = GetPath();
-        Assert.AreEqual(new Vector3( 6.0f, -2.5f, 10.0f), path.Last().position, "Should be the right target.");
-    }
+  [UnityTest]
+  public IEnumerator IssueSide000_000_ShouldFind () {
+    yield return LoadScene ("issue-side-000.000");
+    Assert.IsTrue (HasPath (), "Should find a path.");
+  }
 
-    [UnityTest]
-    public IEnumerator IssueSide000_000_ShouldFind()
-    {
-        yield return LoadScene("issue-side-000.000");
-        Assert.IsTrue(HasPath(), "Should find a path.");
-    }
+  [UnityTest]
+  public IEnumerator IssueStairs000_000_ShouldFind () {
+    yield return LoadScene ("issue-stairs-000.000");
+    Assert.IsTrue (HasPath (), "Should find a path.");
+  }
 
-    [UnityTest]
-    public IEnumerator IssueStairs000_000_ShouldFind()
-    {
-        yield return LoadScene("issue-stairs-000.000");
-        Assert.IsTrue(HasPath(), "Should find a path.");
-    }
+  [UnityTest]
+  public IEnumerator IssueDoor000_000_ShouldFind () {
+    yield return LoadScene ("issue-door-000.000");
+    Assert.IsTrue (HasPath (), "Should find a path.");
+  }
 
-    [UnityTest]
-    public IEnumerator IssueDoor000_000_ShouldFind()
-    {
-        yield return LoadScene("issue-door-000.000");
-        Assert.IsTrue(HasPath(), "Should find a path.");
-    }
+  [UnityTest]
+  public IEnumerator IssueDoor000_001_ShouldFind () {
+    yield return LoadScene ("issue-door-000.001");
+    var path = GetPath ();
+    Assert.AreEqual (new Vector3 (-1.0f, -1.0f, -3.5f), path.Last ().position, "Should be the right target.");
+  }
 
-    [UnityTest]
-    public IEnumerator IssueDoor000_001_ShouldFind()
-    {
-        yield return LoadScene("issue-door-000.001");
-        var path = GetPath();
-        Assert.AreEqual(new Vector3(-1.0f, -1.0f, -3.5f), path.Last().position, "Should be the right target.");
-    }
+  [UnityTest]
+  public IEnumerator IssueStairsDiagonal000_000_ShouldFind () {
+    yield return LoadScene ("issue-stairs2-000.000");
+    Assert.IsTrue (HasPath (), "Should find a path.");
+  }
 
-    [UnityTest]
-    public IEnumerator IssueStairsDiagonal000_000_ShouldFind()
-    {
-        yield return LoadScene("issue-stairs2-000.000");
-        Assert.IsTrue(HasPath(), "Should find a path.");
-    }
+  [UnityTest]
+  public IEnumerator IssueTwistedBlock000_000_ShouldFind () {
+    yield return LoadScene ("issue-twisted-block-000.000");
+    var path = GetPath ();
+    Assert.AreEqual (new Vector3 (-7.0f, -1.0f, -3.5f), path.Last ().position, "Should be the right target.");
+  }
 
-    [UnityTest]
-    public IEnumerator IssueTwistedBlock000_000_ShouldFind()
-    {
-        yield return LoadScene("issue-twisted-block-000.000");
-        var path = GetPath();
-        Assert.AreEqual(new Vector3(-7.0f, -1.0f, -3.5f), path.Last().position, "Should be the right target.");
-    }
+  [UnityTest]
+  public IEnumerator IssueArchBlock000_000_ShouldFind () {
+    yield return LoadScene ("issue-arch-block-000.000");
+    var path = GetPath ();
+    Assert.AreEqual (new Vector3 (-3.5f, -3.0f, -3.0f), path.Last ().position, "Should be the right target.");
+  }
 
-    [UnityTest]
-    public IEnumerator IssueArchBlock000_000_ShouldFind()
-    {
-        yield return LoadScene("issue-arch-block-000.000");
-        var path = GetPath();
-        Assert.AreEqual(new Vector3(-3.5f, -3.0f, -3.0f), path.Last().position, "Should be the right target.");
-    }
+  IEnumerator LoadScene (string sceneName) {
+    SceneManager.LoadScene (sceneName);
+    yield return null;
+  }
 
-	IEnumerator LoadScene(string sceneName) {
-		SceneManager.LoadScene(sceneName);
-		yield return null;
-	}
+  bool HasPath () {
 
-	bool HasPath(){
+    PathFinder pathFinder = new PathFinder ();
 
-		PathFinder pathFinder = new PathFinder();
+    var player = GameObject.Find ("Player").GetComponent<PlayerComponent> ();
+    Assert.IsNotNull (player, "Player not found.");
+    var touchPoint = GameObject.Find ("TouchPoint");
+    Assert.IsNotNull (touchPoint, "TouchPoint not found.");
 
-		var player = GameObject.Find("Player").GetComponent<PlayerComponent>();
-		Assert.IsNotNull(player, "Player not found.");
-		var touchPoint = GameObject.Find("TouchPoint");
-		Assert.IsNotNull(touchPoint, "TouchPoint not found.");
+    var touchPointPos = Camera.main.WorldToScreenPoint (touchPoint.transform.position);
 
-		var touchPointPos = Camera.main.WorldToScreenPoint(touchPoint.transform.position);
+    var pathFound = pathFinder.MovePlayerTo (
+      player.transform.position,
+      touchPointPos,
+      Utility.CleanNormal (player.transform.up)
+    );
 
-		var pathFound = pathFinder.MovePlayerTo(
-			player.transform.position,
-			touchPointPos,
-			Utility.CleanNormal( player.transform.up )
-		);
+    return pathFound;
 
-		return pathFound;
+  }
 
-	}
+  List<PathPoint> GetPath () {
 
-	List<PathPoint> GetPath(){
+    PathFinder pathFinder = new PathFinder ();
 
-		PathFinder pathFinder = new PathFinder();
+    var player = GameObject.Find ("Player");
+    Assert.IsNotNull (player, "Player not found.");
+    var touchPoint = GameObject.Find ("TouchPoint");
+    Assert.IsNotNull (touchPoint, "TouchPoint not found.");
 
-		var player = GameObject.Find("Player");
-		Assert.IsNotNull(player, "Player not found.");
-		var touchPoint = GameObject.Find("TouchPoint");
-		Assert.IsNotNull(touchPoint, "TouchPoint not found.");
+    var touchPointPos = Camera.main.WorldToScreenPoint (touchPoint.transform.position);
 
-		var touchPointPos = Camera.main.WorldToScreenPoint(touchPoint.transform.position);
+    var pathFound = pathFinder.MovePlayerTo (
+      player.transform.position,
+      touchPointPos,
+      Utility.CleanNormal (player.transform.up)
+    );
 
-		var pathFound = pathFinder.MovePlayerTo(
-			player.transform.position,
-			touchPointPos,
-            Utility.CleanNormal(player.transform.up)
-		);
+    Assert.IsTrue (pathFound, "Should find a path.");
 
-		Assert.IsTrue( pathFound, "Should find a path." );
+    return pathFinder.path;
 
-		return pathFinder.path;
-
-	}
+  }
 }
