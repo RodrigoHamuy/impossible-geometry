f919f6ec5b74fd5025b9b98154eaedb07f46957e
oyar <hamuyrodrigo@gmail.com>
2018-06-29 19:37:01 +0100

    Add: Transition liquid shader

---
+++ Assets/assets/postProcessing/disolve/DisolveScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DisolveScript : MonoBehaviour {
+
+	public Material material;
+
+	void OnRenderImage (RenderTexture input, RenderTexture output) {
+
+		Graphics.Blit (input, output, material);
+
+	}
+
+}
