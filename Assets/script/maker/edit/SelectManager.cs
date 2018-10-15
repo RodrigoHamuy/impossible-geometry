4b44fd14111e3a59d473eb24f3f572ca034d7aba
oyar <hamuyrodrigo@gmail.com>
2018-10-15 18:33:52 +0100

    Working: Done: Rotate

--- Assets/script/maker/edit/SelectManager.cs
+++ Assets/script/maker/edit/SelectManager.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,29 +8,64 @@ public class SelectManager : MonoBehaviour {
 
   public Button RemoveBlockBtn;
   public Button RotateBlockBtn;
+  public RotateComponent rotateComponent;
+  public Transform rotateComponentHolder;
+
+  bool isRotating = false;
 
   Renderer target;
   MakerActionsManager manager;
   Color targetOriginalColor;
   TouchComponent touchComponent;
 
+  List<Button> allButtons;
+  List<bool> allButtonsState;
+
   void Start () {
 
+    allButtons = GameObject.FindObjectsOfType<Button> ().ToList ();
     manager = GameObject.FindObjectOfType<MakerActionsManager> ();
     touchComponent = GetComponent<TouchComponent> ();
     touchComponent.onTouchEnd.AddListener (Select);
     RemoveBlockBtn.onClick.AddListener (RemoveBlock);
+    RotateBlockBtn.onClick.AddListener (ShowRotationUi);
+
+    rotateComponent.onRotationStart.AddListener (() => {
+
+      allButtonsState = allButtons.ConvertAll (b => b.interactable);
+      foreach (var btn in allButtons) {
+        btn.interactable = false;
+      }
+      isRotating = true;
+
+    });
+
+    rotateComponent.onRotationDone.AddListener (() => {
+
+      for (int i = 0; i < allButtons.Count; i++) {
+
+        allButtons[i].interactable = allButtonsState[i];
+
+      }
+
+      isRotating = false;
+
+    });
+
     ClearTarget ();
 
   }
 
   void Select (Vector2 touchPos) {
 
+    if (isRotating) return;
+
+    ClearTarget ();
+
     var block = Utility.GetBlocksOnTapPos (touchPos);
 
     if (block) {
 
-      ClearTarget ();
 
       target = block.GetComponent<Renderer> ();
       targetOriginalColor = target.material.color;
@@ -48,10 +85,17 @@ public class SelectManager : MonoBehaviour {
 
   void ClearTarget () {
 
-    if (target) target.material.color = targetOriginalColor;
+    if (target) {
+
+      target.material.color = targetOriginalColor;
+      target.transform.parent = null;
+
+    }
+
     target = null;
     RemoveBlockBtn.interactable = false;
     RotateBlockBtn.interactable = false;
+    rotateComponent.gameObject.SetActive (false);
 
   }
 
@@ -59,6 +103,14 @@ public class SelectManager : MonoBehaviour {
 
     if (target) manager.RemoveBlock (target.transform);
     ClearTarget ();
+
+  }
+
+  void ShowRotationUi () {
+
+    rotateComponent.transform.position = target.transform.position;
+    target.transform.parent = rotateComponentHolder;
+    rotateComponent.gameObject.SetActive (true);
 
   }
 
