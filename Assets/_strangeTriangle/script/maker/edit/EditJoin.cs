2fa084fe8d879e8c5488cd4f8881caad5c8a0e22
oyar <hamuyrodrigo@gmail.com>
2018-12-06 13:09:11 +0000

    Add: Join Horizontally

--- Assets/_strangeTriangle/script/maker/edit/EditJoin.cs
+++ Assets/_strangeTriangle/script/maker/edit/EditJoin.cs
@@ -60,9 +60,24 @@ public class EditJoin : MonoBehaviour {
 
     Clear ();
 
-    joinBlocksToggle.gameObject.SetActive (
-      (!editManager.selectMultiple && editManager.selected.Count == 1)
-    );
+    if (editManager.selected.Count != 1) {
+
+      joinBlocksToggle.gameObject.SetActive (false);
+      return;
+
+    }
+
+    var target = editManager.selected[0].GetTarget ();
+    var targetData = target.GetComponent<EditableBlock> ().data;
+
+    if (targetData.blockType == MakerBlockType.Names.RotateHandleMaker) {
+
+      joinBlocksToggle.gameObject.SetActive (false);
+      return;
+
+    }
+
+    joinBlocksToggle.gameObject.SetActive (true);
 
   }
 
@@ -157,21 +172,140 @@ public class EditJoin : MonoBehaviour {
     var target = editManager.selected[0].GetTarget ();
     Vector2 targetScreenPos = Camera.main.WorldToScreenPoint (target.position);
     var yMargin = CalculateYMargin (target.position, targetScreenPos);
-    // var yRightAngle = CalculateRightYAngle (target.position, targetScreenPos);
-    // var yForwardAngle = CalculateForwardYAngle (target.position, targetScreenPos);
+    var yRightAngle = CalculateRightYAngle (target.position, targetScreenPos);
+    var yForwardAngle = CalculateForwardYAngle (target.position, targetScreenPos);
 
     Vector2 currScreenPos = Camera.main.WorldToScreenPoint (block.position);
     var diff = targetScreenPos - currScreenPos;
-    // var currAngle = Vector2.Angle (targetScreenPos - currScreenPos, Vector2.up);
+    var currAngle = Vector2.Angle (targetScreenPos - currScreenPos, Vector2.up);
 
     if (Mathf.Abs (diff.x) < yMargin) {
 
       JoinBlocksVertically (target.position, block.position);
 
+    } else if (
+      (
+        currAngle < yRightAngle + .001f &&
+        currAngle > yRightAngle - .001f
+      ) ||
+      (
+        currAngle < yForwardAngle + .001f &&
+        currAngle > yForwardAngle - .001f
+      )
+    ) {
+
+      JoinBlocksHorizontally (target.position, block.position);
+
     }
   }
 
+  void JoinBlocksHorizontally (Vector3 start, Vector3 end) {
+
+    var startInEndPlane = projectPosAtPlane (end, Vector3.up, start);
+
+    var endAtStartPlane = projectPosAtPlane (start, Vector3.up, end);
+
+    var startToEnd = end - startInEndPlane;
+    var startToEndDir = startToEnd.normalized;
+    var distance = startToEnd.magnitude;
+
+    var middle = Mathf.Round (distance * .5f);
+
+    for (int i = 0; i < distance; i++) {
+
+      var isMiddle = i == middle;
+
+      if (isMiddle) {
+
+        var startRotation = Quaternion.LookRotation (startToEnd, Vector3.up);
+        var endRotation = Quaternion.LookRotation (-startToEnd, Vector3.up);
+        var nextPosStartPlane = start + startToEndDir * (i + 1);
+        var nextPosEndPlane = startInEndPlane + startToEndDir * (i + 1);
+
+        actionsManager.AddBlock (
+          new MakerAction (
+            MakerActionType.Add,
+            null,
+            HalfCubeMaker,
+            nextPosStartPlane,
+            Vector3.one,
+            startRotation,
+            world
+          ),
+          false
+        );
+
+        actionsManager.AddBlock (
+          new MakerAction (
+            MakerActionType.Add,
+            null,
+            HalfCubeMaker,
+            nextPosEndPlane,
+            Vector3.one,
+            endRotation,
+            world
+          ),
+          false
+        );
+
+      } else {
+
+        Vector3 nextPos;
+
+        if (i < middle) {
+
+          nextPos = start + startToEndDir * (i + 1);
+
+        } else {
+
+          nextPos = startInEndPlane + startToEndDir * (i + 1);
+
+        }
+
+        actionsManager.AddBlock (
+          new MakerAction (
+            MakerActionType.Add,
+            null,
+            CubeMaker,
+            nextPos,
+            Vector3.one,
+            Quaternion.identity,
+            world
+          ),
+          false
+        );
+
+      }
+
+    }
+
+  }
+
+  static Vector3 projectPosAtPlane (Vector3 origin, Vector3 normal, Vector3 posWorld) {
+
+    var posScreen = Camera.main.WorldToScreenPoint (posWorld);
+
+    var posRay = Camera.main.ScreenPointToRay (posScreen);
+
+    var plane = new Plane (normal, origin);
+
+    float posDist;
+
+    plane.Raycast (posRay, out posDist);
+
+    return posRay.origin + posRay.direction * posDist;
+
+  }
+
   void JoinBlocksVertically (Vector3 from, Vector3 to) {
+
+    if (from.y > to.y) {
+
+      var temp = to;
+      to = from;
+      from = to;
+
+    }
 
     var fromScreen = Camera.main.WorldToScreenPoint (from);
     var toScreen = Camera.main.WorldToScreenPoint (to);
